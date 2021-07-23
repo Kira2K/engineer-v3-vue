b3f9b8f607163fbdc138781663439c702c91afbe
Yury Tvardovsky <oziabr@gmail.com>
2021-07-23 16:29:55 +0300

    fixed frontend configuration

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const debug = require('debug')
 const express =  require('express')
 const methodOverride = require('method-override')
@@ -13,6 +14,8 @@ const db = require('./db/models/')
 
 const port = process.env.PORT || 8090
 const frontPort = process.env.FRONT_PORT || 8091
+const backendAddr = process.env.BACKEND_ADDR || `http://localhost:${port}`
+const frontDebug = process.env.FRONT_DEBUG
 
 const app = express()
 const front = express()
@@ -40,6 +43,7 @@ front.get('/', (req, res) => {
 
 front.use((req, res, next) => {
   res.locals.query = req.query
+  res.locals.env = { backendAddr, frontDebug }
   next()
 })
 
@@ -71,79 +75,79 @@ app.use('/api/*', (err, req, res, next) => {
 
 front.get('/:module/:action/:id', async (req, res, next) => {
   const { module, action, id } = req.params
-  const rest = await fetch(`http://localhost:${port}/api/${module}/${id}`).then(res => res.json())
+  const rest = await fetch(`${backendAddr}/api/${module}/${id}`).then(res => res.json())
   res.locals.instance = rest
   next()
 })
 
 front.get('/nomenclature/:action/:id?', async (req, res, next) => {
   const { module, action, id } = req.params
-  res.locals.parent = await fetch(`http://localhost:${port}/api/nomenclaturemodel`).then(res => res.json())
-  res.locals.unit = await fetch(`http://localhost:${port}/api/unit`).then(res => res.json())
-  res.locals.nomenclatureparameter = await fetch(`http://localhost:${port}/api/nomenclatureparameter`).then(res => res.json())
-  res.locals.enabledparameters = !id ? [] : await fetch(`http://localhost:${port}/api/enabledparameter?filter=%7b"nomenclatureId":${id}%7d`).then(res => res.json())
+  res.locals.parent = await fetch(`${backendAddr}/api/nomenclaturemodel`).then(res => res.json())
+  res.locals.unit = await fetch(`${backendAddr}/api/unit`).then(res => res.json())
+  res.locals.nomenclatureparameter = await fetch(`${backendAddr}/api/nomenclatureparameter`).then(res => res.json())
+  res.locals.enabledparameters = !id ? [] : await fetch(`${backendAddr}/api/enabledparameter?filter=%7b"nomenclatureId":${id}%7d`).then(res => res.json())
   next()
 })
 
 front.get('/nomenclaturemodel/:action/:id?', async (req, res, next) => {
-  res.locals.parent = await fetch(`http://localhost:${port}/api/nomenclaturetype`).then(res => res.json())
+  res.locals.parent = await fetch(`${backendAddr}/api/nomenclaturetype`).then(res => res.json())
   next()
 })
 
 front.get('/nomenclaturetype/:action/:id?', async (req, res, next) => {
-  res.locals.parent = await fetch(`http://localhost:${port}/api/nomenclaturegroup`).then(res => res.json())
+  res.locals.parent = await fetch(`${backendAddr}/api/nomenclaturegroup`).then(res => res.json())
   next()
 })
 
 front.get('/nomenclaturegroup/:action/:id?', async (req, res, next) => {
-  res.locals.parent = await fetch(`http://localhost:${port}/api/nomenclatureclass`).then(res => res.json())
+  res.locals.parent = await fetch(`${backendAddr}/api/nomenclatureclass`).then(res => res.json())
   next()
 })
 
 front.get('/nomenclatureparameter/:action/:id?', async (req, res, next) => {
-  res.locals.unit = await fetch(`http://localhost:${port}/api/unit`).then(res => res.json())
+  res.locals.unit = await fetch(`${backendAddr}/api/unit`).then(res => res.json())
   next()
 })
 
 front.get('/enabledparameter/:action/:id?', async (req, res, next) => {
-  res.locals.nomenclature = await fetch(`http://localhost:${port}/api/nomenclature`).then(res => res.json())
-  res.locals.nomenclatureparameter = await fetch(`http://localhost:${port}/api/nomenclatureparameter`).then(res => res.json())
+  res.locals.nomenclature = await fetch(`${backendAddr}/api/nomenclature`).then(res => res.json())
+  res.locals.nomenclatureparameter = await fetch(`${backendAddr}/api/nomenclatureparameter`).then(res => res.json())
   next()
 })
 
 front.get('/value/:action/:id?', async (req, res, next) => {
-  res.locals.enabledparameter = await fetch(`http://localhost:${port}/api/enabledparameter`).then(res => res.json())
-  res.locals.passport = await fetch(`http://localhost:${port}/api/passport`).then(res => res.json())
+  res.locals.enabledparameter = await fetch(`${backendAddr}/api/enabledparameter`).then(res => res.json())
+  res.locals.passport = await fetch(`${backendAddr}/api/passport`).then(res => res.json())
   next()
 })
 
 front.get('/part/:action/:id?', async (req, res, next) => {
-  res.locals.nomenclature = await fetch(`http://localhost:${port}/api/nomenclature`).then(res => res.json())
-  res.locals.passport = await fetch(`http://localhost:${port}/api/passport`).then(res => res.json())
+  res.locals.nomenclature = await fetch(`${backendAddr}/api/nomenclature`).then(res => res.json())
+  res.locals.passport = await fetch(`${backendAddr}/api/passport`).then(res => res.json())
   next()
 })
 
 front.get('/commission/:action/:id?', async (req, res, next) => {
-  res.locals.branch = await fetch(`http://localhost:${port}/api/branch`).then(res => res.json())
-  res.locals.passport = await fetch(`http://localhost:${port}/api/passport`).then(res => res.json())
+  res.locals.branch = await fetch(`${backendAddr}/api/branch`).then(res => res.json())
+  res.locals.passport = await fetch(`${backendAddr}/api/passport`).then(res => res.json())
   next()
 })
 
 front.get('/toro/:action/:id?', async (req, res, next) => {
-  res.locals.repair_type = await fetch(`http://localhost:${port}/api/repair_type`).then(res => res.json())
-  res.locals.malfunction_type = await fetch(`http://localhost:${port}/api/malfunction_type`).then(res => res.json())
-  res.locals.branch = await fetch(`http://localhost:${port}/api/branch`).then(res => res.json())
-  res.locals.passport = await fetch(`http://localhost:${port}/api/passport`).then(res => res.json())
+  res.locals.repair_type = await fetch(`${backendAddr}/api/repair_type`).then(res => res.json())
+  res.locals.malfunction_type = await fetch(`${backendAddr}/api/malfunction_type`).then(res => res.json())
+  res.locals.branch = await fetch(`${backendAddr}/api/branch`).then(res => res.json())
+  res.locals.passport = await fetch(`${backendAddr}/api/passport`).then(res => res.json())
   next()
 })
 
 front.get('/passport/:action/:id?', async (req, res, next) => {
   const { module, action, id } = req.params
-  res.locals.nomenclature = await fetch(`http://localhost:${port}/api/nomenclature`).then(res => res.json())
-  res.locals.counterparty = await fetch(`http://localhost:${port}/api/counterparty`).then(res => res.json())
+  res.locals.nomenclature = await fetch(`${backendAddr}/api/nomenclature`).then(res => res.json())
+  res.locals.counterparty = await fetch(`${backendAddr}/api/counterparty`).then(res => res.json())
   if (id) {
-    res.locals.enabledparameters = await fetch(`http://localhost:${port}/api/enabledparameter?filter=%7b"nomenclatureId":${res.locals.instance.nomenclatureId}%7d`).then(res => res.json())
-    res.locals.value = await fetch(`http://localhost:${port}/api/value?filter=%7b"passportId":${id}%7d`).then(res => res.json())
+    res.locals.enabledparameters = await fetch(`${backendAddr}/api/enabledparameter?filter=%7b"nomenclatureId":${res.locals.instance.nomenclatureId}%7d`).then(res => res.json())
+    res.locals.value = await fetch(`${backendAddr}/api/value?filter=%7b"passportId":${id}%7d`).then(res => res.json())
   }
   next()
 })
@@ -170,7 +174,7 @@ front.get('/:module/:action?/:id?', async (req, res) => {
 front.post('/:module/edit/:id?', async (req, res, next) => {
   const { module, id } = req.params
   debug('form', req.path, JSON.stringify(req.body))
-  const result = await fetch(`http://localhost:${port}/api/${module}/${id ? id : ''}`, {
+  const result = await fetch(`${backendAddr}/api/${module}/${id ? id : ''}`, {
     method: id ? 'put' : 'post',
     body: JSON.stringify(req.body),
     headers: { 'Content-Type': 'application/json' },
@@ -182,16 +186,16 @@ front.post('/:module/edit/:id?', async (req, res, next) => {
   }
   const instanceId = (await result.json()).id
   if (module == 'nomenclature') {
-    const enabledparameters = !id ? [] : (await fetch(`http://localhost:${port}/api/enabledparameter?filter=%7b"nomenclatureId":${id}%7d`).then(res => res.json()))
+    const enabledparameters = !id ? [] : (await fetch(`${backendAddr}/api/enabledparameter?filter=%7b"nomenclatureId":${id}%7d`).then(res => res.json()))
     const requested = Object.keys(req.body).filter(el => el.startsWith('nomenclatureparameter_')).map(el => parseInt(el.replace(/^nomenclatureparameter_/, '')))
     const deleted = enabledparameters.filter(el => !requested.includes(el.nomenclatureParameterId))
     const added = requested.filter(el => !enabledparameters.find(ep => ep.nomenclatureParameterId == el))
-    await Promise.all(added.map(id => fetch(`http://localhost:${port}/api/enabledparameter`, {
+    await Promise.all(added.map(id => fetch(`${backendAddr}/api/enabledparameter`, {
       method: 'post',
       body: JSON.stringify({nomenclatureId: instanceId, nomenclatureParameterId: id}),
       headers: { 'Content-Type': 'application/json' },
     }).then(res => res.json())))
-    await Promise.all(deleted.map(el => console.log(`http://localhost:${port}/api/enabledparameter/${el.id}`) || fetch(`http://localhost:${port}/api/enabledparameter/${el.id}`, { method: 'delete' }).then(res => res.json())))
+    await Promise.all(deleted.map(el => console.log(`${backendAddr}/api/enabledparameter/${el.id}`) || fetch(`${backendAddr}/api/enabledparameter/${el.id}`, { method: 'delete' }).then(res => res.json())))
 
   }
   return res.redirect(302, `/${module}`)
@@ -199,7 +203,7 @@ front.post('/:module/edit/:id?', async (req, res, next) => {
 
 front.post('/:module/delete/:id', async (req, res, next) => {
   const { module, id } = req.params
-  const result = await fetch(`http://localhost:${port}/api/${module}/${id}`, {
+  const result = await fetch(`${backendAddr}/api/${module}/${id}`, {
     method: 'delete'
   })
   if (result.ok) {
